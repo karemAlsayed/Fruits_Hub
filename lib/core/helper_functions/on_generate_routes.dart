933c9d2373e6783519a7024d811e0dae889a4975
karemAlsayed <krym86756@gmail.com>
2024-08-11 09:18:50 +0300

    splash view done

---
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/features/Splash/presentation/views/splash_view.dart';
+import 'package:fruit_hub/features/on_boarding/presentation/views/on_boarding_view.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) 
+{
+
+  switch (settings.name){
+
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
+
+
+    default:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+  }
+
+}
